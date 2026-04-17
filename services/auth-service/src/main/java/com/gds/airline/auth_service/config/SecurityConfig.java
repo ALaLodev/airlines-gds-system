30f14325ffaca274a0c4fa9369ba814c40e03bf8
Antonio <113071717+ALaLodev@users.noreply.github.com>
2026-04-17 19:16:48 +0200

    feats: inicializacion de infraestructura GDS con config-server, eureka y auth-server conectado a BD con Docker

---
+++ services/auth-service/src/main/java/com/gds/airline/auth_service/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.gds.airline.auth_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                //  Desactivamos CSRF porque usaremos tokens JWT en nuestra API REST
+                .csrf(csrf -> csrf.disable())
+
+                //  Reglas de autorización (Por ahora lo abrimos para poder probar la DB)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+
+    // Este Bean le dice a Spring cómo encriptar las contraseñas en la base de datos
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ services/auth-service/src/main/java/com/gds/airline/auth_service/service/CustomUserDetailService.java
@@ -0,0 +1,27 @@
+package com.gds.airline.auth_service.service;
+
+import com.gds.airline.auth_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        return userRepository.findByEmail(email)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getEmail())
+                        .password(user.getPassword())
+                        .authorities(user.getRole().name())
+                                .build())
+                        .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con email" + email));
+    }
+}
